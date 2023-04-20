d06e3be81bde20cd9a928ccecc9cf8e408349623
Ahmet Yılmaz <79370064+ahmetyilmaz34@users.noreply.github.com>
2023-04-21 00:51:23 +0300

    Route mechanism to delete user's card with CardIndex

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -61,7 +61,29 @@ export default (router) => {
         res.status(200).json(deleteResult)
     })
 
-
+    //! Index ile Kart Silme (Sıra ile, hızlı silme mesela ön yüzdeki kart bilgilerini seçerek index numarasına göre siliyoruz.)
+    router.delete("/cards/:cardIndex/delete-by-index", Session, async (req, res) => {
+        if (!req.params?.cardIndex) { // cardIndex gelip gelmediği kontrol ediyoruz. CardIndex gelmez ise hata döndürecek
+            throw new ApiError("Card index is required", 400, "cardIndexRequired");
+        }
+        let cards = await Cards.getUserCards({ // Kullanıcının kartlarını backend de silmek için önce okumak gerekiyor(getUserCards).
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            cardUserKey: req.user?.cardUserKey, // soru işeretini ? işaretini safe call yani güvenli çağırmak için kullanıyoruz bazen undifened olarak değer dönebiliyor  patlamaması için bu şekilde kullanılıyor.
+        })
+        const index = parseInt(req.params?.cardIndex); // indexi integer çeviriyoruz.
+        if (index >= cards?.cardDetails.length) { // index numarası kullanıcının kart sayısından fazla ise hata döndürecek
+            throw new ApiError("Card doesn't exists, check index number", 400, "cardIndexInvalid")
+        }
+        const { cardToken } = cards?.cardDetails[index] // body üzerinden almıyoruz cardTokenı index numarasına göre siliyoruz artık.
+        let deleteResult = await Cards.deleteUserCard({ // iyzico üzerinden gelen cevabı deleteResult değişkeni üzerine atanıyor.
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            cardUserKey: req.user?.cardUserKey,
+            cardToken: cardToken
+        })
+        res.json(deleteResult)
+    })
 
 
 }
