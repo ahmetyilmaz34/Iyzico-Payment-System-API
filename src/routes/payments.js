72bb497a6a28f0f6c0872f23407ef295db267157
Ahmet Yılmaz <79370064+ahmetyilmaz34@users.noreply.github.com>
2023-04-27 21:53:11 +0300

    Normal payment route mechanism using CardToken with registered card

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -264,5 +264,87 @@ export default (router) => {
         res.json(result);
     })
 
+    //! HALİ HAZIRDA VAR OLAN BİR KART İLE  cardtoken KULLANARAK ÖDEME YAPMA
+    router.post("/payments/:cartId/with-registered-card-token", Session, async (req, res) => {
+        let { cardToken } = req.body; // body üzerinden cardToken bilgisini alıyoruz.
+        if (!cardToken) {
+            throw new ApiError("Card token is required", 400, "cardTokenRequired")
+        }
+        if (!req.user?.cardUserKey) {
+            throw new ApiError("No registred card available", 400, "cardUserKeyRequired")
+        }
+        if (!req.params?.cartId) {
+            throw new ApiError("Card id is required", 400, "cardIdRequired")
+        }
+        const cart = await Carts.findOne({ _id: req.params?.cartId }).populate("buyer").populate("products");
+        if (!cart) {
+            throw new ApiError("Cart not found", 400, "cartNotFound")
+        }
+        if (cart?.completed) {
+            throw new ApiError("Cart is Completed", 400, "cartCompleted")
+        }
 
+        const paidPrice = cart.products.map((product) => product.price).reduce((a, b) => a + b, 0);
+
+        const card = {
+            cardToken,
+            cardUserKey: req.user?.cardUserKey
+        }
+        const data = {
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            price: paidPrice,
+            paidPrice: paidPrice,
+            currency: Iyzipay.CURRENCY.TRY,
+            installment: "1",
+            basketId: String(cart?._id),
+            paymentChannel: Iyzipay.PAYMENT_CHANNEL.WEB,
+            paymentGroup: Iyzipay.PAYMENT_GROUP.PRODUCT,
+            paymentCard: card,
+            buyer: {
+                id: String(req.user._id),
+                name: req.user?.name,
+                surname: req.user?.surname,
+                gsmNumber: req.user?.phoneNumber,
+                email: req.user?.email,
+                identityNumber: req.user?.identityNumber,
+                lastLoginDate: moment(req.user?.updateAt).format("YYYY-MM-DD HH:mm:ss"),
+                registrationDate: moment(req.user?.createdAt).format("YYYY-MM-DD HH:mm:ss"),
+                registrationAddress: req.user?.address,
+                ip: req.user?.ip,
+                city: req.user?.city,
+                country: req.user?.country,
+                zipCode: req.user?.zipCode
+            },
+            shippingAddress: {
+                contactName: req.user?.name + " " + req.user?.surname,
+                city: req.user?.city,
+                country: req.user?.country,
+                address: req.user?.address,
+                zipCode: req.user?.zipCode
+            },
+            billingAddress: {
+                contactName: req.user?.name + " " + req.user?.surname,
+                city: req.user?.city,
+                country: req.user?.country,
+                address: req.user?.address,
+                zipCode: req.user?.zipCode
+            },
+            basketItems: cart.products.map((product, index) => {
+                return {
+                    id: String(product?._id),
+                    name: product?.name,
+                    category1: product?.categories[0],
+                    category2: product?.categories[1],
+                    itemType: Iyzipay.BASKET_ITEM_TYPE[product?.itemType],
+                    price: product?.price
+                }
+            })
+        }
+
+        let result = await Payments.createPayment(data);
+        await CompletePayment(result);
+        res.json(result);
+
+    })
 }
