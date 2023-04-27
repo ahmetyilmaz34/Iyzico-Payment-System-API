f3ad8a262f775546c9fda4a1ba9016f544161ce5
Ahmet Yılmaz <79370064+ahmetyilmaz34@users.noreply.github.com>
2023-04-27 21:35:51 +0300

    Normal payment route mechanism using CardIndex with registered card

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -89,7 +89,7 @@ export default (router) => {
         res.json(result);
     })
 
-    
+
     //! YENİ BİR KARTLA ÖDEME OLUŞTUR VE KARTI KAYDET
     router.post("/payments/:cartId/with-new-card/register-card", Session, async (req, res) => {
         const { card } = req.body;
@@ -172,4 +172,97 @@ export default (router) => {
         await CompletePayment(result);
         res.json(result);
     })
+
+    //! HALİ HAZIRDA VAR OLAN BİR KART İLE  cardIndex KULLANARAK ÖDEME YAPMA VE KARTI KAYDETMİYORUZ
+    router.post("/payments/:cartId/:cardIndex/with-registered-card-index", Session, async (req, res) => {
+        let { cardIndex } = req.params; // url üzerinden indexi alıyoruz
+        if (!cardIndex) { // kart index yok ise hata döndürüyoruz.
+            throw new ApiError("Card index is required", 400, "cardIndexRequired")
+        }
+        if (!req.user?.cardUserKey) { //cardUserKey kontrolünü gerçekleştiriyoruz.
+            throw new ApiError("No registred card available", 400, "cardUserKeyRequired")
+        }
+        const cards = await Cards.getUserCards({
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            cardUserKey: req.user?.cardUserKey
+        })
+        const index = parseInt(cardIndex); // cardIndexi integere çeviriyoruz.
+        if (index >= cards?.cardDetails?.length) { // index numarası kart sayısından fazla ise hata verir
+            throw new ApiError("Card doesn't exists", 400, "cardIndexInvalid")
+        }
+        const { cardToken } = cards?.cardDetails[index]; // kart index kullanarak cardtoken bilgisini aldık.
+
+        if (!req.params?.cartId) {
+            throw new ApiError("Card id is required", 400, "cardIdRequired")
+        }
+        const cart = await Carts.findOne({ _id: req.params?.cartId }).populate("buyer").populate("products");
+        if (!cart) {
+            throw new ApiError("Cart not found", 400, "cartNotFound")
+        }
+        if (cart?.completed) {
+            throw new ApiError("Cart is Completed", 400, "cartCompleted")
+        }
+        const paidPrice = cart.products.map((product) => product.price).reduce((a, b) => a + b, 0);
+        const card = { // kart bilgilerini gönderiyoruz.
+            cardToken,
+            cardUserKey: req.user?.cardUserKey
+        }
+        const data = {
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            price: paidPrice,
+            paidPrice: paidPrice,
+            currency: Iyzipay.CURRENCY.TRY,
+            installment: "1",
+            basketId: String(cart?._id),
+            paymentChannel: Iyzipay.PAYMENT_CHANNEL.WEB,
+            paymentGroup: Iyzipay.PAYMENT_GROUP.PRODUCT,
+            paymentCard: card,
+            buyer: {
+                id: String(req.user._id),
+                name: req.user?.name,
+                surname: req.user?.surname,
+                gsmNumber: req.user?.phoneNumber,
+                email: req.user?.email,
+                identityNumber: req.user?.identityNumber,
+                lastLoginDate: moment(req.user?.updateAt).format("YYYY-MM-DD HH:mm:ss"),
+                registrationDate: moment(req.user?.createdAt).format("YYYY-MM-DD HH:mm:ss"),
+                registrationAddress: req.user?.address,
+                ip: req.user?.ip,
+                city: req.user?.city,
+                country: req.user?.country,
+                zipCode: req.user?.zipCode
+            },
+            shippingAddress: {
+                contactName: req.user?.name + " " + req.user?.surname,
+                city: req.user?.city,
+                country: req.user?.country,
+                address: req.user?.address,
+                zipCode: req.user?.zipCode
+            },
+            billingAddress: {
+                contactName: req.user?.name + " " + req.user?.surname,
+                city: req.user?.city,
+                country: req.user?.country,
+                address: req.user?.address,
+                zipCode: req.user?.zipCode
+            },
+            basketItems: cart.products.map((product, index) => {
+                return {
+                    id: String(product?._id),
+                    name: product?.name,
+                    category1: product?.categories[0],
+                    category2: product?.categories[1],
+                    itemType: Iyzipay.BASKET_ITEM_TYPE[product?.itemType],
+                    price: product?.price
+                }
+            })
+        }
+        let result = await Payments.createPayment(data);
+        await CompletePayment(result);
+        res.json(result);
+    })
+
+
 }
