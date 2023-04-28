011f1164dfddeea408a9fe1bf0d9d0c1100dcadc
Ahmet Yılmaz <79370064+ahmetyilmaz34@users.noreply.github.com>
2023-04-28 16:24:44 +0300

    3D Secure payment and card registration forwarding mechanism with a new card

--- src/routes/payments-threeds.js
+++ src/routes/payments-threeds.js
@@ -31,7 +31,7 @@ export default (router) => {
         res.status(200).json(result); // cevap olarak burayı döndürüyoruz.
     })
 
-    //! YENİ BİR KARTLA ÖDEME OLUŞTUR VE KARTI KAYDETMEK - THREEDS
+    //! YENİ BİR KARTLA ÖDEME OLUŞTURMA VE KARTI KAYDETMEMEK - THREEDS
     router.post("/threeds/payments/:cartId/with-new-card", Session, async (req, res) => {
         const { card } = req.body;
         if (!card) {
@@ -107,5 +107,85 @@ export default (router) => {
         const html = Buffer.from(result?.threeDSHtmlContent, 'base64').toString(); // html verisini alıcaz
         res.send(html);
 
+    })
+
+    //! YENİ BİR KARTLA ÖDEME OLUŞTURMA VE KARTI KAYDETMEK - THREEDS
+    router.post("/threeds/payments/:cartId/with-new-card/register-card", Session, async (req, res) => {
+        const { card } = req.body;
+        if (!card) {
+            throw new ApiError("Card is required", 400, "cardRequired");
+        }
+        if (!req.params?.cartId) {
+            throw new ApiError("Cart id is required", 400, "cartIdRequired");
+        }
+        const cart = await Carts.findOne({ _id: req.params?.cartId }).populate("buyer").populate("products");
+        if (!cart) {
+            throw new ApiError("Cart not found", 404, "cartNotFound");
+        }
+        if (cart?.completed) {
+            throw new ApiError("Cart is completed", 400, "cartCompleted");
+        }
+        if (req.user.cardUserKey) {
+            card.cardUserKey = req.user?.cardUserKey;
+        }
+
+        card.registerCard = "1";
+        const paidPrice = cart.products.map((product) => product.price).reduce((a, b) => a + b, 0);
+
+        const data = {
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            price: paidPrice,
+            paidPrice: paidPrice,
+            currency: Iyzipay.CURRENCY.TRY,
+            installments: '1',
+            basketId: String(cart?._id),
+            paymentChannel: Iyzipay.PAYMENT_CHANNEL.WEB,
+            paymentGroup: Iyzipay.PAYMENT_GROUP.PRODUCT,
+            callbackUrl: `${process.env.END_POINT}/threeds/payments/complete`,
+            paymentCard: card,
+            buyer: {
+                id: String(req.user._id),
+                name: req.user?.name,
+                surname: req.user?.surname,
+                gsmNumber: req.user?.phoneNumber,
+                email: req.user?.email,
+                identityNumber: req.user?.identityNumber,
+                lastLoginDate: moment(req.user?.updatedAt).format("YYYY-MM-DD HH:mm:ss"),
+                registrationDate: moment(req.user?.createdAt).format("YYYY-MM-DD HH:mm:ss"),
+                registrationAddress: req.user?.address,
+                ip: req.user?.ip,
+                city: req.user?.city,
+                country: req.user?.country,
+                zipCode: req.user?.zipCode
+            },
+            shippingAddress: {
+                contactName: req.user?.name + " " + req.user?.surname,
+                city: req.user?.city,
+                country: req.user?.country,
+                address: req.user?.address,
+                zipCode: req.user?.zipCode
+            },
+            billingAddress: {
+                contactName: req.user?.name + " " + req.user?.surname,
+                city: req.user?.city,
+                country: req.user?.country,
+                address: req.user?.address,
+                zipCode: req.user?.zipCode
+            },
+            basketItems: cart.products.map((product, index) => {
+                return {
+                    id: String(product?._id),
+                    name: product?.name,
+                    category1: product.categories[0],
+                    category2: product.categories[1],
+                    itemType: Iyzipay.BASKET_ITEM_TYPE[product?.itemType],
+                    price: product?.price
+                }
+            })
+        }
+        let result = await PaymentsThreeDS.initializePayment(data);
+        const html = Buffer.from(result?.threeDSHtmlContent, 'base64').toString();
+        res.send(html);
     })
 }
